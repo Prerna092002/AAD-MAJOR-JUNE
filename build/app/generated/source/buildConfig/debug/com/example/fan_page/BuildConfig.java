ca32a641702259d7f9d71fd7e9185e093ac88e55
Prerna <prerna3573.be20@chitkaraedu.in>
2021-08-13 17:45:53 +0530

    Yeah its my commit

--- build/app/generated/source/buildConfig/debug/com/example/fan_page/BuildConfig.java
+++ build/app/generated/source/buildConfig/debug/com/example/fan_page/BuildConfig.java
@@ -5,7 +5,7 @@ package com.example.fan_page;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.company.aad minor project";
+  public static final String APPLICATION_ID = "com.example.fan_page";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0.0";
